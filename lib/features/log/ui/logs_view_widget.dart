906cc9e3106a4652a46d97fd080a7638ee7e6d17
Codica <codica@arcor.de>
2026-08-15 02:23:36 +0200

    WIP: just a widget showing a text

---
+++ lib/features/log/ui/logs_view_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LogsViewWidget extends StatelessWidget {
+  const LogsViewWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Text("LogsViewer");
+  }
+}
